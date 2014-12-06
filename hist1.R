5fa14f1dc213507fcdb3ee62518253ff56501e69
mvonderlieth <mark.vonderlieth@gmail.com>
2014-12-05 18:15:19 -0800

    Temp save

--- hist1.R
+++ hist1.R
@@ -1,5 +1,5 @@
 # author mvonderlieth
-library(dplyr)
+library(dplyr,warn.conflicts = F)
 library(tidyr)
 library(lubridate)
 library(ggplot2)
@@ -7,13 +7,12 @@ library(ggplot2)
 # main logic is in the main() function at the end of file
 ## support functions
 # make plot
-makePlot <- function(d,mn,md) {
+plotHistMeanStepsPerDay <- function(d,mn,md) {
     meanText = paste("Mean =", as.integer(mn))
     medianText = paste("Median =", as.integer(md))
-    xxx = d[[1,3]]
-    textDate = ymd(analysisData$date[1])
-    textMedianY = max(analysisData$stepsTotal)
-    textMeanY = textMedianY - 1000
+    textDate = ymd(d$date[1])
+    textMedianY = max(d$stepsTotal)
+    textMeanY = textMedianY - (textMedianY * .075)
     
     p = ggplot(d, aes(x=datePosix, y=stepsTotal)) +
         #         geom_smooth() +
@@ -21,7 +20,7 @@ makePlot <- function(d,mn,md) {
         geom_histogram(stat="identity", color="black", fill="grey", alpha=I(.67)) +
         geom_abline(slope = 0, intercept = mn, color = "red") +
         geom_abline(slope = 0, intercept = md, color = "blue") +
-        labs(list(x ="Date",y ="Number of Steps", title="Total Steps Per Day")) +
+        labs(list(x ="Date",y ="Number of Steps", title="Total Steps Per Day (NA's not removed)")) +
         annotate("text", label = medianText, x = textDate, y = textMedianY, size = 4, colour = "blue", adj = 0) +
         annotate("text", label = meanText, x = textDate, y = textMeanY, size = 4, colour = "red", adj = 0)
     
@@ -30,8 +29,8 @@ makePlot <- function(d,mn,md) {
 }
 
 # plot on screen
-plotOnScreen <- function(d,mn,md) {
-    p = makePlot(d,mn,md)
+plotOnScreen <- function(plotFunction,...) {
+    p = plotFunction(...)
     print(p)
     return(p)
 }
@@ -44,7 +43,7 @@ plotOnDevice <- function(p) {
 }
 
 # build the data set we need
-buildData <- function(dataFromSource) {
+buildNAData <- function(dataFromSource) {
     d = dataFromSource %>%
         group_by(date) %>%
         summarise(stepsTotal = sum(steps,na.rm=TRUE)) %>%
@@ -59,20 +58,21 @@ buildNoNAData <- function(dataFromSource) {
     return(d)
 }
 
-# load the data into tmp if not there already,
-#  and once it's there, if not test use full data, else just use sample data
-loadCsvData <- function(dataFile,test) {
+# load the full data source into a 'tmp' if not there already,
+#  and once it's there, if not test, use full data, else just use sample of the full data.
+# Note also storing into globals so that this method can easily be called repetitively.
+loadCsvData <- function(dataFile,test,testSampleSize) {
     if (!exists("FullDataFromSource")) {
         FullDataFromSource <<- read.csv(dataFile)
     }
     
-    # for design phase just test with small data set
+    # for expolatory phase just test with small data set in case full data set is large
     if (!test) {
         WorkingDataFromSource <<- FullDataFromSource
     } else {
         # try to keep the same sample set
         if (!exists("WorkingDataFromSource_sample")) {
-            WorkingDataFromSource_sample <<- FullDataFromSource %>% sample_n(10)
+            WorkingDataFromSource_sample <<- FullDataFromSource %>% sample_n(testSampleSize)
             WorkingDataFromSource <<- WorkingDataFromSource_sample
         }
     }
@@ -81,30 +81,31 @@ loadCsvData <- function(dataFile,test) {
 ## main
 main <- function() {
     # when testing data may not represent full set of data.
+    # set test to TRUE when exploring data
     dataFile = "./activity.csv"
     test = FALSE
     
     if (file.exists(dataFile)) {
         # load data as globals
-        loadCsvData(dataFile,test)
+        loadCsvData(dataFile,test,1000)
         
         #         par(mfrow = c(2, 1), mar = c(4, 4, 2, 1))
         
         #build analyis data
         # want this to be a global so I can view it after a run
-        analysisData <<- buildData(WorkingDataFromSource)
-        stepsMean = mean(analysisData$stepsTotal, na.rm=TRUE)
-        stepsMedian = median(analysisData$stepsTotal, na.rm=TRUE)
+        analysisNAData <<- buildNAData(WorkingDataFromSource)
+        stepsMean = mean(analysisNAData$stepsTotal, na.rm=TRUE)
+        stepsMedian = median(analysisNAData$stepsTotal, na.rm=TRUE)
         
         #plot on screen
-        p = plotOnScreen(analysisData, stepsMean, stepsMedian)
+        p = plotOnScreen(plotHistMeanStepsPerDay, analysisNAData, stepsMean, stepsMedian)
         
         #         analysisNoNAData <<- buildNoNAData(WorkingDataFromSource)
         #         analysisData <<- buildData(analysisNoNAData)
         #         p = plotOnScreen(analysisData)
         
         # create plot on file device, close when done
-        plotOnDevice(p)
+#         plotOnDevice(p)
         
         #         par(mfrow = c(1, 1))
     }
@@ -131,3 +132,6 @@ main()
 # dim(stepsOrdered)
 # stepsOrdered[15264/2,]
 #t = WorkingDataFromSource %>% arrange(interval) %>% group_by(interval) %>% summarise(count=n(), intervalStepsMean=mean(steps, na.rm=TRUE), intervalStepsMedian=median(steps, na.rm=TRUE), totalSteps=sum(steps,na.rm=TRUE))
+# pairs(airquality)
+# fit = lm(Ozone ~ Wind + Solar.R + Temp, data = airquality)
+#left off at 2:35
